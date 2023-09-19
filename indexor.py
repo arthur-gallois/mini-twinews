2a2b62ae1ae21121aae85a800d3d6833c7163281
arthur <arthur.gallois@protonmail.com>
2023-09-19 11:40:45 +0200

    Commentaire sur indexor.py et utils.py

--- indexor.py
+++ indexor.py
@@ -1,34 +1,39 @@
-import os
-from pymongo import MongoClient
-import json
-import bz2
-from tqdm import tqdm
-import multiprocessing
-from joblib import Parallel, delayed
+import os  
+from pymongo import MongoClient 
+import json 
+import bz2  
+from tqdm import tqdm  
+import multiprocessing  
+from joblib import Parallel, delayed  
 
+# Obtenir le nombre de cœurs CPU disponibles sur la machine
 num_cores = multiprocessing.cpu_count()
 
+# Obtenir le chemin du répertoire racine
 DIR_PATH = os.path.split(os.path.abspath(__file__))[0]
-NEWS_PATH = os.path.join(DIR_PATH,"news")
-USERS_PATH = os.path.join(DIR_PATH,"users")
 
+# Définir les chemins vers les répertoires contenant les données à importer
+NEWS_PATH = os.path.join(DIR_PATH, "news")
+USERS_PATH = os.path.join(DIR_PATH, "users")
+
+# Établir une connexion à la base de données MongoDB (local par défaut)
 client = MongoClient()
 db = client["twitternewsrec"]
 news_collection = db["news"]
-
 users_collection = db["users"]
 
+# Supprimer les collections existantes dans la base de données
 news_collection.drop()
 users_collection.drop()
 
-news_collection.create_index("lastUrlDomain",unique = False)
-news_collection.create_index("url",unique = True)
-users_collection.create_index("user_id",unique = True)
+# Créer des index pour accélérer les opérations de recherche
+news_collection.create_index("lastUrlDomain", unique=False)
+news_collection.create_index("url", unique=True)
+users_collection.create_index("user_id", unique=True)
 
-
-
+# Fonction pour importer des données JSON depuis un fichier
 def import_news(filename):
-    filepath = os.path.join(NEWS_PATH,filename)
+    filepath = os.path.join(NEWS_PATH, filename)
     with bz2.open(filepath) as file:
         for line in file.readlines():
             try:
@@ -37,29 +42,34 @@ def import_news(filename):
                 data = json.loads(line.decode('utf-8'))
             news_collection.insert_one(data)
 
+# Fonction pour importer des données JSON d'utilisateurs depuis un fichier
 def import_users(filename):
-        filepath = os.path.join(USERS_PATH,filename)
-        with bz2.open(filepath) as file:
-            for line in file.readlines():
-                try:
-                    data = json.loads(line)
-                except:
-                    data = json.loads(line.decode('utf-8'))
-                users_collection.insert_one(data)
+    filepath = os.path.join(USERS_PATH, filename)
+    with bz2.open(filepath) as file:
+        for line in file.readlines():
+            try:
+                data = json.loads(line)
+            except:
+                data = json.loads(line.decode('utf-8'))
+            users_collection.insert_one(data)
 
 print("Importing news...")
 news_filenames = tqdm(os.listdir(NEWS_PATH))
-processed_list = Parallel(n_jobs=num_cores,backend='threading')(delayed(import_news)(filename) for filename in news_filenames)
+
+# Importer les données des actualités en parallèle
+processed_list = Parallel(n_jobs=num_cores, backend='threading')(delayed(import_news)(filename) for filename in news_filenames)
 
 print("Importing users...")
 users_filenames = tqdm(os.listdir(USERS_PATH))
-processed_list = Parallel(n_jobs=num_cores,backend='threading')(delayed(import_users)(filename) for filename in users_filenames)
 
-
+# Importer les données d'utilisateurs en parallèle
+processed_list = Parallel(n_jobs=num_cores, backend='threading')(delayed(import_users)(filename) for filename in users_filenames)
 
 print("Mapping users to news...")
 newsUsersMapping = dict()
 users_id = tqdm(users_collection.distinct("user_id"))
+
+# Fonction pour créer la correspondance entre les utilisateurs et les actualités
 def setNewsUsersMapping(id):
     data = users_collection.find_one({"user_id": id})
     for news in data["news"]:
@@ -67,17 +77,21 @@ def setNewsUsersMapping(id):
             newsUsersMapping[news] = []
         newsUsersMapping[news].append(id)
 
-processed_list = Parallel(n_jobs=num_cores,backend='threading')(delayed(setNewsUsersMapping)(user_id) for user_id in users_id)    
+# Créer la correspondance en parallèle
+processed_list = Parallel(n_jobs=num_cores, backend='threading')(delayed(setNewsUsersMapping)(user_id) for user_id in users_id)
 
 print("Inserting mapping in the database...")
 mappings = tqdm(newsUsersMapping.items())
+
+# Fonction pour insérer la correspondance entre les utilisateurs et les actualités dans la base de données
 def insertNewsUsersMapping(mapping):
-    url,ids = mapping
+    url, ids = mapping
     news_collection.update_one({"url": url}, {"$set": {"users": ids}})
-     
-processed_list = Parallel(n_jobs=num_cores,backend='threading')(delayed(insertNewsUsersMapping)(mapping) for mapping in mappings)    
-    
-print("Done !")
+
+# Insérer la correspondance en parallèle
+processed_list = Parallel(n_jobs=num_cores, backend='threading')(delayed(insertNewsUsersMapping)(mapping) for mapping in mappings)
+
+print("Done!")
 news_count = news_collection.count_documents({})
 users_count = users_collection.count_documents({})
 print(f"Number of news: {news_count} | Number of users: {users_count}")
--- utils.py
+++ utils.py
@@ -1,86 +1,109 @@
-from pymongo import MongoClient
-from tqdm import tqdm
+from pymongo import MongoClient  # Importer la bibliothèque MongoDB pour la connexion à la base de données
+from tqdm import tqdm  # Importer la bibliothèque pour afficher des barres de progression
 
-import numpy as np
+import numpy as np  # Importer la bibliothèque NumPy pour le traitement de données
+
+# Établir une connexion à la base de données MongoDB (localhost par défaut)
 client = MongoClient('localhost')
 
-
-
+# Sélectionner la base de données "twitternewsrec"
 db = client["twitternewsrec"]
+
+# Sélectionner les collections "news" et "users" dans la base de données
 news_collection = db["news"]
 users_collection = db["users"]
+
+# Compter le nombre de documents dans les collections "news" et "users"
 news_count = news_collection.count_documents({})
 users_count = users_collection.count_documents({})
 
-
+# Définir une classe "News" pour représenter les données des actualités
 class News():
-    def __init__(self,url,text,users):
+    def __init__(self, url, text, users):
         self.url = url
         self.text = text
         self.users = users
-    def __repr__(self) -> str:
-        return(f"News object [url: {self.url}, text: {self.text[:100]}..., users: {self.users if self.users else None}]")
     
+    def __repr__(self) -> str:
+        return f"News object [url: {self.url}, text: {self.text[:100]}..., users: {self.users if self.users else None}]"
+
+# Fonction pour obtenir une liste d'actualités à partir de la base de données
 def get_news_list():
     news_list = []
     cursor = news_collection.find()
-    print("Loading news from mongodb...")
+    print("Loading news from MongoDB...")
+    
+    # Afficher une barre de progression basée sur le nombre de documents
     with tqdm(total=news_count) as pbar:
         while cursor.alive:
             for doc in cursor:
                 pbar.update(1)
-                news_list.append(News(doc['url'],doc['scrap']['text'],doc['users'] if 'users' in doc else None))    
+                news_list.append(News(doc['url'], doc['scrap']['text'], doc['users'] if 'users' in doc else None))
+    
     return news_list
 
+# Définir une classe "Users" pour représenter les données des utilisateurs
 class Users():
     TRAIN_TEST_RATIO = 0.8
-    def __init__(self,user_id,news_url):
+    
+    def __init__(self, user_id, news_url):
         self.user_id = user_id
         self.news_url = news_url
         self._get_news_text()
         n = len(self.news_text)
-        self.train_list = self.news_text[:int(n*self.TRAIN_TEST_RATIO)]
-        self.test_list=self.news_text[int(n*self.TRAIN_TEST_RATIO):]
+        self.train_list = self.news_text[:int(n * self.TRAIN_TEST_RATIO)]
+        self.test_list = self.news_text[int(n * self.TRAIN_TEST_RATIO):]
 
     def _get_news_text(self):
         self.news_text = [None for _ in self.news_url]
-        for i,url in enumerate(self.news_url):
-            news=news_collection.find_one({"url":url})
-            if(news is None):
+        for i, url in enumerate(self.news_url):
+            news = news_collection.find_one({"url": url})
+            if news is None:
                 self.news_text[i] = None
                 continue
             
             self.news_text[i] = news['scrap']['text']
 
-        self.news_url = [news for i,news in enumerate(self.news_url) if self.news_text[i] is not None]
+        self.news_url = [news for i, news in enumerate(self.news_url) if self.news_text[i] is not None]
         self.news_text = [text for text in self.news_text if text is not None]
 
-    def get_test_batch(self,n_batch = 100, read_ratio=0.1):
-        n_read = min(int(n_batch * 0.1),len(self.test_list))
+    def get_test_batch(self, n_batch=100, read_ratio=0.1):
+        """
+        Cette méthode génère un lot de test pour un utilisateur, composé d'une combinaison d'actualités lues et non lues.
+        
+        :param n_batch: Le nombre total d'actualités à inclure dans le lot de test.
+        :param read_ratio: Le ratio d'actualités lues dans le lot de test (valeur entre 0 et 1).
+        
+        :return: Deux listes - la première contient le texte des actualités du lot de test, la seconde indique si chaque actualité est lue (1) ou non lue (0).
+        """
+        n_read = min(int(n_batch * read_ratio), len(self.test_list))
         n_unread = n_batch - n_read
 
-        test_list_text = list(np.random.choice(self.test_list,n_read,replace=False))
+        test_list_text = list(np.random.choice(self.test_list, n_read, replace=False))
         test_list_isread = [1 for _ in range(n_read)]
 
         query = {"scrap.text": {"$nin": self.news_text}}  
         result = [news['scrap']['text'] for news in (news_collection.find(query))]
-        chosen_result_indices = list(np.random.choice(len(result),n_unread,replace=False))
+        chosen_result_indices = list(np.random.choice(len(result), n_unread, replace=False))
         test_list_text += [result[i] for i in chosen_result_indices]
         test_list_isread += [0 for _ in range(n_unread)]
-        return test_list_text,test_list_isread
-        
+        return test_list_text, test_list_isread
+
+# Fonction pour obtenir une liste d'utilisateurs à partir de la base de données
 def get_users_list():
     users_list = []
     cursor = users_collection.find()
-    print("Loading users from mongodb...")
+    print("Loading users from MongoDB...")
+    
+    # Afficher une barre de progression basée sur le nombre de documents
     with tqdm(total=users_count) as pbar:
         while cursor.alive:
             for user in cursor:
                 pbar.update(1)
-                new_user = Users(user["user_id"],user['news'])
-                users_list.append(new_user)  
-
+                new_user = Users(user["user_id"], user['news'])
+                users_list.append(new_user)
+    
     return users_list
 
-
-users = get_users_list()    
+# Obtenir la liste d'utilisateurs à partir de la base de données
+users = get_users_list()
